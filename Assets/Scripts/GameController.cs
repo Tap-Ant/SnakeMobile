a6cc2ae0f1c3ac04e77d08c7ae389ae47744a5aa
Tapio Anttila <tapio-a@hotmail.com>
2024-06-24 12:31:45 +0300

    Snake First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+
+    const float width = 3.7f;
+    const float height = 7f;
+    public float snakeSpeed = 1;
+
+    public BodyPart bodyPrefab = null;
+    public GameObject rockPrefab = null;
+    public GameObject eggPrefab = null;
+    public GameObject goldEggPrefab = null;
+
+    public Sprite tailSprite = null;
+    public Sprite bodySprite = null;
+    public SnakeHead snakeHead = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        Debug.Log("Starting Snake Game");
+        CreateWalls();
+        StartGame();
+        CreateEgg();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        snakeHead.ResetSnake();
+    }
+
+    void CreateWalls()
+    {
+        Vector3 start  = new Vector3(-width, -height, 0);
+        Vector3 finish = new Vector3(-width, +height, 0);
+        CreateWall(start, finish);
+
+        start = new Vector3(+width, -height, 0);
+        finish = new Vector3(+width, +height, 0);
+        CreateWall(start, finish);
+
+        start = new Vector3(-width, -height, 0);
+        finish = new Vector3(+width, -height, 0);
+        CreateWall(start, finish);
+
+        start = new Vector3(-width, +height, 0);
+        finish = new Vector3(+width, +height, 0);
+        CreateWall(start, finish);
+    }
+
+    void CreateWall(Vector3 start, Vector3 finish)
+    {
+        float distance = Vector3.Distance(start, finish);
+        int noOfRocks = (int)(distance * 3);
+        Vector3 delta = (finish - start) / noOfRocks;
+
+        Vector3 position = start;
+        for(int rock = 0; rock <= noOfRocks; rock++)
+        {
+            float rotation = Random.Range(0, 360);
+            float scale    = Random.Range(1.5f, 2f);
+            CreateRock(position, scale, rotation);
+            position = position + delta;
+        }
+    }
+
+    void CreateRock(Vector3 position, float scale, float rotation)
+    {
+        GameObject rock = Instantiate(rockPrefab, position, Quaternion.Euler(0,0,rotation));
+        rock.transform.localScale = new Vector3(scale, scale, 1);
+    }
+
+    void CreateEgg(bool golden = false)
+    {
+        Vector3 position;
+        position.x = -width + Random.Range(1f, (width * 2) - 2f);
+        position.y = -height + Random.Range(1f, (height * 2) - 2f);
+        position.z = 0;
+        if (golden)
+            Instantiate(goldEggPrefab, position, Quaternion.identity);
+        else
+            Instantiate(eggPrefab, position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : BodyPart
+{
+    Vector2 movement;
+    private BodyPart tail = null;
+
+    const float TIMETOADDBODYPART = 0.1f;
+    float addTimer = TIMETOADDBODYPART;
+    public int partsToAdd = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwipeControls.OnSwipe += SwipeDetection;
+    }
+
+    // Update is called once per frame
+    override public void Update()
+    {
+        base.Update();
+
+        SetMovement(movement);
+        UpdateDirection();
+        UpdatePosition();
+
+        if (partsToAdd > 0)
+        {
+            addTimer -= Time.deltaTime;
+            if (addTimer <= 0 )
+            {
+                addTimer = TIMETOADDBODYPART;
+                AddBodyPart();
+                partsToAdd--;
+            }
+        }
+    }
+
+    void AddBodyPart()
+    {
+        if (tail == null)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z = newPosition.z + 0.01f;
+
+            BodyPart newPart = Instantiate(GameController.instance.bodyPrefab, newPosition, Quaternion.identity);
+            newPart.following = this;
+            tail = newPart;
+            newPart.TurnIntoTail();
+        }
+        else
+        {
+            Vector3 newPosition = tail.transform.position;
+            newPosition.z = newPosition.z + 0.01f;
+            BodyPart newPart = Instantiate(GameController.instance.bodyPrefab, newPosition, Quaternion.identity);
+            newPart.following = tail;
+            newPart.TurnIntoTail();
+            tail.TurnIntoBodyPart();
+            tail = newPart;
+        }
+    }
+
+    void SwipeDetection(SwipeControls.SwipeDirection direction)
+    {
+        switch(direction)
+        {
+            case SwipeControls.SwipeDirection.Up:
+                MoveUp();
+                break;
+            case SwipeControls.SwipeDirection.Down:
+                MoveDown();
+                break;
+            case SwipeControls.SwipeDirection.Left:
+                MoveLeft();
+                break;
+            case SwipeControls.SwipeDirection.Right:
+                MoveRight();
+                break;
+        }
+    }
+
+    void MoveUp()
+    {
+        movement = Vector2.up * GameController.instance.snakeSpeed * Time.deltaTime;
+    }
+
+    void MoveDown()
+    {
+        movement = Vector2.down * GameController.instance.snakeSpeed * Time.deltaTime;
+    }
+
+    void MoveLeft()
+    {
+        movement = Vector2.left * GameController.instance.snakeSpeed * Time.deltaTime;
+    }
+
+    void MoveRight()
+    {
+        movement = Vector2.right * GameController.instance.snakeSpeed * Time.deltaTime;
+    }
+
+    public void ResetSnake()
+    {
+        tail = null;
+        MoveUp();
+        partsToAdd = 5;
+        addTimer = TIMETOADDBODYPART;
+    }
+}
